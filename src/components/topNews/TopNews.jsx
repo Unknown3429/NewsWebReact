edded2aba47a4c9034239dbb71b6dd9b9d75b979
Unknown3424 <ps4342565@gmail.com>
2023-08-09 19:10:47 +0530

    vercel

--- src/components/topNews/TopNews.jsx
+++ src/components/topNews/TopNews.jsx
@@ -30,48 +30,49 @@ const TopNews = ({ news, title, news2 }) => {
     return (
         <div>
             {/* <!-- Top News Start--> */}
-            {!loading ? (<div className="top-news">
-                <div className="container-fluid">
-                    <h1 >{title} News</h1>
-                    <div className="row">
-                        <div onClick={() => handleSinglePage(firstNews2)} className="col-md-6 tn-left">
-                            <div className="tn-img">
-                                <img className='main-img' src={firstNews2?.imageUrl} />
-                                <div className="tn-content">
-                                    <div className="tn-content-inner">
-                                        <NavLink className="tn-date" ><i className="far fa-clock"></i>{dayjs(firstNews2?.createdAt
-                                        ).format(
-                                            "MMM D, YYYY"
-                                        )}</NavLink>
-                                        <NavLink to={`/singlepage/${firstNews2?.title}`} className="tn-title" href="">{firstNews2?.title}</NavLink>
+            {!loading ?
+                (<div className="top-news">
+                    <div className="container-fluid">
+                        <h1 >{title} News</h1>
+                        <div className="row">
+                            <div onClick={() => handleSinglePage(firstNews2)} className="col-md-6 tn-left">
+                                <div className="tn-img main">
+                                    <img className='main-img' src={firstNews2?.imageUrl} />
+                                    <div className="tn-content">
+                                        <div className="tn-content-inner">
+                                            <NavLink className="tn-date" ><i className="far fa-clock"></i>{dayjs(firstNews2?.createdAt
+                                            ).format(
+                                                "MMM D, YYYY"
+                                            )}</NavLink>
+                                            <NavLink to={`/singlepage/${firstNews2?.title}`} className="tn-title" href="">{firstNews2?.title}</NavLink>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-md-6 tn-right">
-                            <div className="row">
-                                {sliceNewsTop2?.map((data, i) => {
-                                    return (
-                                        <div key={i} className="col-md-6">
-                                            <div className="tn-img">
-                                                {!data?.imageUrl ? <img className='newsimg' src={newsimg} /> : <img className='newsimg' src={data?.imageUrl} />}
-                                                <div onClick={() => handleSinglePage(data)} className="tn-content">
-                                                    <div className="tn-content-inner">
-                                                        <NavLink className="tn-date" href=""><i className="far fa-clock"></i>{dayjs(data?.createdAt).format(
-                                                            "MMM D, YYYY"
-                                                        )}</NavLink>
-                                                        <NavLink to={`/singlepage/${data?.title}`} className="tn-title" href="">{data?.title}</NavLink>
+                            <div className="col-md-6 tn-right">
+                                <div className="tn-right-inner row">
+                                    {sliceNewsTop2?.map((data, i) => {
+                                        return (
+                                            <div key={i} className="col-md-6">
+                                                <div className="tn-img item">
+                                                    {!data?.imageUrl ? <img className='newsimg' src={newsimg} /> : <img className='newsimg' src={data?.imageUrl} />}
+                                                    <div onClick={() => handleSinglePage(data)} className="tn-content">
+                                                        <div className="tn-content-inner">
+                                                            <NavLink className="tn-date" href=""><i className="far fa-clock"></i>{dayjs(data?.createdAt).format(
+                                                                "MMM D, YYYY"
+                                                            )}</NavLink>
+                                                            <NavLink to={`/singlepage/${data?.title}`} className="tn-title" href="">{data?.title}</NavLink>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    )
-                                })}
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>)
+                </div>)
                 : (
                     <div className="loadingSkeleton ">
                         <img className='text-center' src={loader} alt="Loading...." />
